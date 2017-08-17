18990f955248d66a70d4a24db9da6d194dd25b40
Reymark C. Urbano (KPPH-RPB7N0070D) <PB7N0070@calcomp.com.tw>
2017-08-17 14:36:56 +0800

    third commit

--- src/kramyer/urbano/examples/ConfirmDialogPlain.java
+++ src/kramyer/urbano/examples/ConfirmDialogPlain.java
@@ -13,7 +13,7 @@ import kramyer.urbano.dialogs.CustomDialog;
  */
 public class ConfirmDialogPlain {
     public static void main(String[] args) {
-        String Message = "Do you like this Custom Dialog huh?";
+        String Message = "Do you like this Custom Dialog?";
         
         int Choice = CustomDialog.showConfirmMessageBox(null, Message);
         
